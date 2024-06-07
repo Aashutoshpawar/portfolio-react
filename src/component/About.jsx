b83c9ce79bf00438f02e77409a6db9bc1a80fec3
Aashutosh Rajendra Pawar <aashutoshpawar123@gmail.com>
2024-06-07 23:22:04 +0530

    Update About.jsx

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Exp from './Exp'
 import Certifications from './Certifications'
 import Project from './Project'
-import 1 from './assets/laptopimg.jpg';
+
 
 
 
@@ -18,7 +18,7 @@ function About() {
                 </p>
             <div id='aboutcontainer' className='w-screen mt-[2rem]  lg:flex lg:justify-center '>
                 <div id='left1' className=' mb-[1rem] flex justify-center '>
-                    <img src={1} className='reletive h-[15rem] w-[15rem] rounded-lg lg:h-[25rem] lg:w-[25rem]' />
+                    <img src='./src/assets/laptopimg.jpg' className='reletive h-[15rem] w-[15rem] rounded-lg lg:h-[25rem] lg:w-[25rem]' />
                 </div>
                 <div id='right' className=' '>
                     <p className=' ml-5 mr-5 text-[1.5rem] text-slate-600 border-4 rounded-lg p-[1rem]  lg:h-[25rem] lg:w-[40rem]'>
