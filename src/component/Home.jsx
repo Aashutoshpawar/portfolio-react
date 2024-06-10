7268eff371926e08e41de8468cf2d515e4ec8cbc
Aashutosh Rajendra Pawar <aashutoshpawar123@gmail.com>
2024-06-11 04:40:46 +0530

    Update Home.jsx

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -19,7 +19,7 @@ function Home() {
         <div className='select-none overflow-hidden'>
             <div className='h-full w-full lg:flex mt-[9rem]'>
                 <div id='left' className='flex justify-center lg:w-full lg:justify-end lg:pr-[5rem]' >
-                    <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Ficonduck.com%2Fillustrations%2F106905%2Fweb-developer&psig=AOvVaw0cZHYJppDTCgV6W4sgPZYu&ust=1718147138361000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCOi9-IST0oYDFQAAAAAdAAAAABAS" className='h-[26rem] w-[19rem] border-2 ' />
+                    <img src="https://github.com/Aashutoshpawar/portfolio-react/blob/main/src/component/myimg.png" className='h-[26rem] w-[19rem] border-2 ' />
                 </div>
 
                 <div id='right' className='lg:w-full '>
